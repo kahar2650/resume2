0d3c737f35c85e9b2d1aa8fd130e6808bbbd892c
resume2 <kahar.char77@gmail.com>
2024-05-31 21:45:27 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,143 @@
+// Send Email
+const form = document.querySelector('form');
+const fullname = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodymessage = `Full Name: ${fullname.value}<br> Email: ${email.value}
+    <br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+
+    Email.send({
+        SecureToken : "6c959efc-d940-48d7-a1c7-0376070644bd",
+        To : 'kahar.char77@gmail.com',
+        From : "kahar.char77@gmail.com",
+        Subject : subject.value,
+        Body : bodymessage
+    }).then(
+      message => {
+        if (message == "OK") {
+            Swal.fire({
+                title: "Succes!",
+                text: "Message Sent Successfully!",
+                icon: "success"
+              });
+            } 
+        }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector("error-txt.email");
+    
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Enter a valid email address";
+        }
+        else {
+            errorTxtEmail.innerText = "Email Address can't be blank";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!fullname.classList.contains("error") && !email.classList.contains
+        ("error") && !phone.classList.contains("error") && !subject.classList.
+        contains("error") && !mess.classList.contains("error")) {
+            sendEmail();
+
+            form.reset();
+            return false;
+        }
+});
+
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // Active Section for Animation on Scroll
+            sec.classList.add('show-animate');
+        }
+            // If Want To Use Animation That Repeats on Scroll Use This
+            else {
+                sec.classList.remove('show-animate');
+            }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+
+}
